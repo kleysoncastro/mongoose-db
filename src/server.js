94ac5a16431210c87161e9b14abf63c7f201178f
kleyson castro <kleysoncastro99@gamil.com>
2019-09-24 12:34:18 -0300

    criacao do modulo produtoQualquer, e inset no mongoDB

--- src/server.js
+++ src/server.js
@@ -3,7 +3,22 @@ mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedT
 
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'erro de conexao'));
+db.on('error', console.error.bind(console, 'Erro ao tentar se conectar ao mongoDB'));
 db.once('open', function(){
     console.log("conexao bem feita");
+});
+
+// objeto produto, sera transformado em modulo pela funcao mongoose.model()
+var produtoSchema = new mongoose.Schema( {nome: String});
+
+    // modulo para produtoSchema, recebe dois parmetros: 1 nome da  "Collection", 2 modelo Schema
+    var produtoQualquer = mongoose.model('produtoDB', produtoSchema);
+
+// produto especifico, instancia do objeto produtoQualquer
+var refri = new produtoQualquer({nome: 'Fanta'});
+
+refri.save().then(()=>{
+    console.log('dados gravado com sucesso ');
+}).catch((erro)=>{
+    console.log("Erro na gravacao "+ erro);
 });
